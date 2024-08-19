db0367d0d1538493abcd1d73bc424bd968b7b28a
Sivavishnu <sivavishnu2424@gmail.com>
2024-08-19 22:45:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,12 @@ const bodyparser = require('body-parser')
 const app = express();
 const port = 3000;
 
-// Middleware to parse JSON bodies
 app.use(express.json());
 app.use(bodyparser.urlencoded({extended:true}))
 
-// Set the views directory and EJS as the view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Route to display data from JSON file
 app.get('/', (req, res) => {
   const filePath = path.join(__dirname, 'data.json');
 
@@ -29,15 +26,13 @@ app.get('/', (req, res) => {
 app.post('/data', (req, res) => {
     const filePath = path.join(__dirname, 'data.json');
     
-    // Read the existing data from the file
     fs.readFile(filePath, 'utf8', (err, fileData) => {
       if (err) {
         return res.status(500).json({ error: 'Failed to read file' });
       }
       
       let dataArray = JSON.parse(fileData);
-      
-      // Get new data from the form
+
       const newData = {
         name: req.body.name,
         email: req.body.email,
@@ -47,19 +42,17 @@ app.post('/data', (req, res) => {
       console.log("new name == "+ newData.name)
       
       dataArray.push(newData);
-      
-      // Write updated data back to the file
+
       fs.writeFile(filePath, JSON.stringify(dataArray, null, 2), 'utf8', (err) => {
         console.log("success !")
         if (err) {
           return res.status(500).json({ error: 'Failed to write file' });
         }
-        res.redirect('/'); // Redirect to the home page to see the updated data
+        res.redirect('/');
       });
     });
   });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server running at ${port}`);
 });
